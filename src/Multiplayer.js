fa271924266632332c94db9c8f162f48dfb07e51
prerana-56 <preranapokarna@gmail.com>
2026-01-09 20:06:37 +0530

    Update Multiplayer.js

--- src/Multiplayer.js
+++ src/Multiplayer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./index.css";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://tt-server-zk2w.onrender.com");
 function Square({value, onSquareClick, clr}) {
   const buttonStyle = {
     color: clr // Applying the color to the text inside the button
@@ -246,5 +246,6 @@ function Board() {
 }
 
 export default Board
+
 
 
